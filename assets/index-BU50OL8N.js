881075a772c8a56db048e355d852390f43606248
wei-zuan <269530115+wei-zuan@users.noreply.github.com>
2026-04-19 15:03:19 +0000

    feat: sync full build assets with experiment PK

---
+++ assets/index-BU50OL8N.js
@@ -0,0 +1,19 @@
+import{d3 as m,c_ as c,d0 as f,d1 as d}from"./index-C4pOqYWx.js";import{n as u}from"./class-map-Ds3M0WS9.js";import{o as b}from"./if-defined-BHf-TP8j.js";import{c as h}from"./index-BQSWIpxL.js";import"./index-BC6f5own.js";const v=m`
+  :host {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+  }
+`;var o=function(l,i,r,s){var a=arguments.length,e=a<3?i:s===null?s=Object.getOwnPropertyDescriptor(i,r):s,n;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(l,i,r,s);else for(var p=l.length-1;p>=0;p--)(n=l[p])&&(e=(a<3?n(e):a>3?n(i,r,e):n(i,r))||e);return a>3&&e&&Object.defineProperty(i,r,e),e};let t=class extends f{constructor(){super(...arguments),this.disabled=!1}render(){return d`
+      <wui-input-text
+        type="email"
+        placeholder="Email"
+        icon="mail"
+        size="lg"
+        .disabled=${this.disabled}
+        .value=${this.value}
+        data-testid="wui-email-input"
+        tabIdx=${b(this.tabIdx)}
+      ></wui-input-text>
+      ${this.templateError()}
+    `}templateError(){return this.errorMessage?d`<wui-text variant="sm-regular" color="error">${this.errorMessage}</wui-text>`:null}};t.styles=[c,v];o([u()],t.prototype,"errorMessage",void 0);o([u({type:Boolean})],t.prototype,"disabled",void 0);o([u()],t.prototype,"value",void 0);o([u()],t.prototype,"tabIdx",void 0);t=o([h("wui-email-input")],t);
